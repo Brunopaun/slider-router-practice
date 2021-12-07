018492ebbd260aa0953278e1166da1bede9d2c39
Brunopaun <brunopaun0@gmail.com>
2021-12-07 10:51:59 -0300

    adding a README file

---
+++ README.md
@@ -0,0 +1,8 @@
+//--frontendmentor challenge--//
+
+This website was created to practice some features like slider, routing, and react lazy. The challenge didn't specify what technologies could be used, instead, it mentioned that could be great trying to build the website using a framework and any technologies that would enhance the learning.  
+
+So, we used react-responsive-carousel to set up our sliders which was a good practice of making use of libs. To practice a bit of sass, we developed the whole CSS with SASS using the modules approach.
+
+--Learnings
+When talking about responsiveness, I find complicated the use of sass as the only technology for building its CSS which could be sorted of with Tailwind. The project needs some improvements, most related to the implementation of react-responsive-carousel and its styles. 
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 import "./Layout.module.scss";
+import Spinner from '../Spinner/Spinner';
 
-import Home from '../../pages/Home';
-import Navigation from '../Navigation/Navigation'
-import Crew from '../../pages/Crew';
-import Destination from '../../pages/Destination';
-import Technology from '../../pages/Technology';
+const Navigation = React.lazy(()=>import('../Navigation/Navigation'));
+const Crew = React.lazy(()=> import('../../pages/Crew'));
+const Destination = React.lazy(()=> import('../../pages/Destination'));
+const Technology = React.lazy(()=> import('../../pages/Technology'));
+const Home = React.lazy(()=>import('../../pages/Home'));
 
 const Layout = () =>{
 
     return (
         <div className="layout">
-            <Navigation></Navigation>
-            <main>
-                <Routes>
-                    <Route path='*' element={<Navigate replace to="/home"/>}/>
-                    <Route path='/home' element={<Home/>} />
-                    <Route path='/crew' element={<Crew/>} />
-                    <Route path='/destination' element={<Destination/>} />
-                    <Route path='/technology' element={<Technology/>} />                
-                </Routes>
-            </main>
+            <Suspense fallback={Spinner}>
+                <Navigation></Navigation>
+                <main>
+                    <Routes>    
+                            <Route path='*' element={<Navigate replace to="/home"/>}/>
+                            <Route path='/home' element={<Home/>} />
+                            <Route path='/crew' element={<Crew/>} />
+                            <Route path='/destination' element={<Destination/>} />
+                            <Route path='/technology' element={<Technology/>} />                
+                   </Routes>
+                </main>
+            </Suspense>
         </div>
     )
 };
